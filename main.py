1bc43f3b9438c8b86748b0cabce47a19def32180
fatihk25 <fatihkhoiri25@gmail.com>
2022-06-06 18:03:21 +0700

    add find commodity and order

--- main.py
+++ main.py
@@ -20,3 +20,18 @@ async def load_data(page_num: int = 1, page_size: int = 10):
     start = (page_num - 1) * page_size
     end = start + page_size
     return data[start:end]
+
+@app.get("/data/{order_by}")
+async def get_data(order_by: str = "TransactionDate", page_num: int = 1, page_size: int = 10):
+    data = df.sort_values(by=[f'{order_by}'], ascending=False).to_dict(orient="records")
+    start = (page_num - 1) * page_size
+    end = start + page_size
+    return data[start:end]
+
+@app.get('/item/{name}')
+async def find_commodity(commodity_name: str,  page_num: int = 1, page_size: int = 10):
+    data = df.loc[df['Commodity'] == commodity_name]
+    data = data.to_dict(orient="records")
+    start = (page_num - 1) * page_size
+    end = start + page_size
+    return data[start:end]
